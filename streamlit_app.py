d549759c0aa5016d0ac3c9eb33b0b5c5d48e2fac
Aghila Unnikrishnan <ayniunniakhila9884@gmail.com>
2022-12-21 15:22:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My Parents Nee Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
