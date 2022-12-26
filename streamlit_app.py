9cb718aec8715dbcbebb9ecb20d592dce45188e8
Aghila Unnikrishnan <ayniunniakhila9884@gmail.com>
2022-12-26 20:04:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,4 +72,4 @@ if streamlit.button('Add a Fruit to the List'):
   streamlit.text(back_from_function)
 #streamlit.write('Thanks for adding  ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
